6a3d1f0eec1c19162684d25e46e38bfaa5d388d5
Harsh <inobeta1by0@gmail.com>
2026-09-18 01:47:42 +0530

    this is the start

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">Welcome to the Blank Landing Page</h1>
+    </main>
+  )
+}
+
+export default Home
